84526ff54bd1e1dcdcf70ce36f225310b85ae08c
Andrij Kulynych <akulmmm@gmail.com>
2019-11-19 14:40:20 +0200

    "Fix 4.4.Max from three numbers[#197362]"

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -2,9 +2,7 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public int max(int first, int second, int third) {
-        int result = first;
-        int max = first > second ? first : (first != second ? second : result);
-        result = max > third ? max : (max != third ? third : first);
-        return result;
+        int max = first > second ? first : second;
+        return third > max ? third : max;
     }
 }
