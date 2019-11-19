82eadcf713e17cba42dd8539f541adc35e1c7875
Andrij Kulynych <akulmmm@gmail.com>
2019-11-19 14:14:06 +0200

    Revert " "Fix2 task 4.3  [#197362]""
    This reverts commit 3580cbe3

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -3,6 +3,8 @@ package ru.job4j.condition;
 public class Max {
     public static int max(int left, int right) {
 
-        return left > right ? left : right;
+        int result = left > right ? left : ((right != left) ? right : left);
+
+        return result;
     }
 }
