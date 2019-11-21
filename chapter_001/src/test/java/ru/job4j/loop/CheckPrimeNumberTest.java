16ebffdb5056714c6eb4d20a03bb720080cc0466
Andrij Kulynych <akulmmm@gmail.com>
2019-11-21 21:31:56 +0200

    PrimeNumber_test49[#197377]

--- chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -14,16 +14,9 @@ public class CheckPrimeNumberTest {
     }
 
     @Test
-    public void when1() {
+    public void when49() {
         CheckPrimeNumber prime = new CheckPrimeNumber();
-        boolean rsl = prime.check(1);
-        assertThat(rsl, is(true));
-    }
-
-    @Test
-    public void when11() {
-        CheckPrimeNumber prime = new CheckPrimeNumber();
-        boolean rsl = prime.check(11);
+        boolean rsl = prime.check(49);
         assertThat(rsl, is(true));
     }
 
