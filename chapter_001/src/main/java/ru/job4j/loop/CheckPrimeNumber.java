179709238750472076c5cbbeb7f6aa8ad785bfeb
Andrij Kulynych <akulmmm@gmail.com>
2019-11-21 17:24:40 +0200

    PrimeNumber fix[#197377]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,11 +3,9 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public boolean check(int num) {
         boolean prime = false;
-        for (int m = 1; m <= num; m++) {
-            if ( m != num && (num % m == 0)) {
-                if(m!=1) {
-                    break;
-                }
+        for (int m = 2; m <= num; m++) {
+            if (m != num && (num % m == 0)) {
+                break;
             } else prime = true;
         }
         return prime;
