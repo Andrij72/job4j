52a45b1aa0cbfb3138643c373007b0489386b4af
Andrij Kulynych <akulmmm@gmail.com>
2019-11-22 12:25:07 +0200

    Small fix primeNumber[#197377]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -5,11 +5,9 @@ public class CheckPrimeNumber {
         boolean prime = false;
         for (int m = 2; m <= num; m++) {
             if ((num % m == 0) && (m != num)) {
-                System.out.printf("<%d> isn't prime number because divide on <%d>%n", num, m);
                 break;
             } else if (m == num) {
                 prime = true;
-                System.out.printf("<%d> is prime number%n", num);
             }
         }
         return prime;
