a676dfb1f79b97251cc20819c05641ef2f43f4fd
Andrij Kulynych <akulmmm@gmail.com>
2019-11-24 14:03:47 +0200

    5.6.Some fix simple numbers[#197375]

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -4,15 +4,11 @@ public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
         for (int x = 2; x <= finish; x++) {
-            for (int m = 2; m <= finish; m++) {
-                if (x % m == 0) {
-                    if (x == m) {
-                        count++;
-                    }
-                    break;
-                }
+            if (new CheckPrimeNumber().check(x)) {
+                count++;
             }
         }
         return count;
     }
 }
+
