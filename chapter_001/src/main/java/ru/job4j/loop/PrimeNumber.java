053eb4d858c4852aa6ba8a68bb1e41b3e9d4293c
Andrij Kulynych <akulmmm@gmail.com>
2019-11-25 18:03:31 +0200

    some fix task PrimeryNumber[#197375]

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -3,8 +3,9 @@ package ru.job4j.loop;
 public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
+        CheckPrimeNumber checkNumber = new CheckPrimeNumber();
         for (int x = 2; x <= finish; x++) {
-            if (new CheckPrimeNumber().check(x)) {
+            if (checkNumber.check(x)) {
                 count++;
             }
         }
