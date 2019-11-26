66a02876f7b09629bdb901f8b7db78d4ef46dbc2
Andrij Kulynych <akulmmm@gmail.com>
2019-11-27 00:04:34 +0200

    fix flip the array[# 197390]

--- chapter_001/src/main/java/ru/job4j/arrays/Turn.java
+++ chapter_001/src/main/java/ru/job4j/arrays/Turn.java
@@ -2,13 +2,11 @@ package ru.job4j.arrays;
 
 public class Turn {
     public int[] back(int[] array) {
-        int endIndex = array.length - 1;
-
-        for (int index = 0; index < endIndex; index++) {
+        int endIndex = array.length;
+        for (int index = 0; index < endIndex/2; index++) {
             int tmp = array[index];
-            array[index] = array[array.length - index - 1];
-            array[array.length - index - 1] = tmp;
-            endIndex--;
+            array[index] = array[endIndex - index - 1];
+            array[endIndex  - index - 1] = tmp;
         }
         return array;
     }
