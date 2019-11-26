7db3b7712e8c8ff51df5ee933bc802e2c9e04f56
Andrij Kulynych <akulmmm@gmail.com>
2019-11-26 12:35:36 +0200

    Fill the array[# 197380]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+import static java.lang.Math.pow;
+
+public class Square {
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int index = 0; index < bound; index++) {
+            rst[index] = (int) pow(index+1, 2);
+        }
+        return rst;
+    }
+}
