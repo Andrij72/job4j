ed8581fb690fb17a4b06e3e89a893bc70d01370f
Andrij Kulynych <akulmmm@gmail.com>
2019-11-28 22:38:40 +0200

    Find min value in array

--- chapter_001/src/main/java/ru/job4j/arrays/Min.java
+++ chapter_001/src/main/java/ru/job4j/arrays/Min.java
@@ -1,13 +1,13 @@
 package ru.job4j.arrays;
 
-    public class Min {
-        public static int findMin(int[] array) {
-            int min = array[0];
-            for (int index = 1; index < array.length; index++) {
-                if (min>array[index]){
-                    min=array[index];
-                }
+public class Min {
+    public static int findMin(int[] array) {
+        int min = array[0];
+        for (int index = 1; index < array.length; index++) {
+            if (min > array[index]) {
+                min = array[index];
             }
-            return min;
         }
+        return min;
     }
+}
