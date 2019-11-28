7c64087481f3e4ff543953b9733f515814dfee69
Andrij Kulynych <akulmmm@gmail.com>
2019-11-28 21:25:25 +0200

    Find index in range.[#197392]

--- chapter_001/src/main/java/ru/job4j/arrays/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/arrays/FindLoop.java
@@ -1,8 +1,7 @@
 package ru.job4j.arrays;
 
 public class FindLoop {
-    public int indexOf(int[] data, int el) {
-
+    public static int indexOf(int[] data, int el) {
         int rst = -1;
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
@@ -11,5 +10,16 @@ public class FindLoop {
             }
         }
         return rst;
+    }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int result = -1;
+        for (int index = start; index < finish; index++) {
+            if (data[index] == el) {
+                result = index;
+                break;
+            }
+        }
+        return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/arrays/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/arrays/FindLoopTest.java
@@ -25,13 +25,27 @@ public class FindLoopTest {
         assertThat(result, is(expect));
     }
 
+
     @Test
-    public void whenArrayHas_9_Then_1() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3, 9};
-        int value = 9;
-        int result = find.indexOf(input, value);
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
         int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+
+    @Test
+    public void WhenNotFind3() {
+        int[] input = new int[] {5, 2, 11, 2, 9};
+        int value = 3;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
         assertThat(result, is(expect));
     }
 }
