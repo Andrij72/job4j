748e421c43bbce48245808d0c502834310aea9fb
Andrij Kulynych <akulmmm@gmail.com>
2019-11-28 17:46:58 +0200

    6.4.1. The word ends with ... [# 197391]

---
+++ chapter_001/src/test/java/ru/job4j/arrays/EndsWithTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.arrays;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class EndsWithTest {
+    @Test
+    public void whenEndWithPostThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'R', 'r'};
+        char[] post = {'R', 'r'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotEndWithPostThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'Z', '0'};
+        boolean result =EndsWith.endsWith(word, post);
+        assertThat(result, is(false));
+    }
+}
