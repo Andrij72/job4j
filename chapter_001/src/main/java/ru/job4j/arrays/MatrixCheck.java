9eadd1733c5e6d5a4bfed363bdd96653e41f009a
Andrij Kulynych <akulmmm@gmail.com>
2019-12-02 17:00:34 +0200

    Winning combinations in Sokoban [# 197385]

---
+++ chapter_001/src/main/java/ru/job4j/arrays/MatrixCheck.java
@@ -0,0 +1,67 @@
+package ru.job4j.arrays;
+
+public class MatrixCheck {
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        int count = 0;
+        char[][] board1 = new char[5][5];
+        board1[1][0] = 'X';
+        board1[1][1] = 'X';
+        board1[1][2] = 'X';
+        board1[1][3] = 'X';
+        board1[1][4] = 'X';
+        board1[0][0] = 'X';
+        board1[0][2] = 'X';
+        board1[1][2] = 'X';
+        board1[2][2] = 'X';
+        board1[3][2] = 'X';
+        board1[4][2] = 'X';
+        for (int row = 0; row < board.length; row++) {
+            for (int cell = 0; cell < board.length; cell++) {
+                char sign = board[row][cell];
+                System.out.print(sign);
+                if (sign == board1[row][cell]) {
+                    count++;
+                }
+            }
+            System.out.println();
+        }
+        if (count == 5) {
+            result = true;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[][] hasWinVertical = {
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+        };
+        boolean win = isWin(hasWinVertical);
+        System.out.println("A board has a winner : " + win);
+        System.out.println();
+        char[][] hasWinHor = {
+                {'_', '_', '_', '_', '_'},
+                {'X', 'X', 'X', 'X', 'X'},
+                {'_', '_', '_', '_', '_'},
+                {'_', '_', '_', '_', '_'},
+                {'_', '_', '_', '_', '_'},
+        };
+        boolean winHor = isWin(hasWinHor);
+        System.out.println("A board has a winner : " + winHor);
+        System.out.println();
+        char[][] notWin = {
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', 'X', '_', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+        };
+        boolean lose = isWin(notWin);
+        System.out.println("A board has a winner : " + lose);
+        System.out.println();
+    }
+}
