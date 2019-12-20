e2fd251b962861adca4f3560fe944b91217972a8
Andrij72 <akulmmm@gmail.com>
2019-12-20 11:47:16 +0200

    1.4. Calling a method with a return type. [# 197514]

---
+++ chapter_001/src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+
+    public String engToRus(String eng) {
+        String tword = "Неизвестное слово: " + eng;
+        System.out.println(tword);
+        return tword;
+    }
+
+
+    public static void main(String[] args) {
+        DummyDic dummyDic = new DummyDic();
+        dummyDic.engToRus("java");
+    }
+}
