6ad8f84740f1431878c6deb75a689f63a44b164c
Andrij72 <akulmmm@gmail.com>
2019-12-24 17:55:21 +0200

    4. The distance between points in 3d space[#197506]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -5,14 +5,35 @@ import static java.lang.StrictMath.pow;
 public class Point {
     private int x;
     private int y;
+    private int z;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-        public double distance(Point that) {
+    public Point(int x, int y, int z) {
+        new Point(x, y);
+        this.z = z;
+    }
+
+    public double distance(Point that) {
         return Math.sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    /**
+     * Method distance3d  calculate distance between points in 3d coordinat sistem.
+     *
+     * @param that
+     * @return
+     */
+    public double distance3d(Point that) {
+        return Math.sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2)
+                + pow(this.z - that.z, 2));
+    }
+
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
     }
 
     public static void main(String[] args) {
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -3,15 +3,25 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+
 public class PointTest {
 
     @Test
     public void distance() {
-        Point first = new Point(1,1);
-        Point second = new Point(3,3);
+        Point first = new Point(1, 1);
+        Point second = new Point(3, 3);
         int expected = 2;
         int delta = 828;
         double out = first.distance(second);
         Assert.assertEquals(expected, out, delta);
+    }
+
+    @Test
+    public void distance3d() {
+        Point first = new Point(1, 1, 1);
+        Point second = new Point(4, 4, 4);
+        double result = first.distance3d(second);
+        Assert.assertThat(result, is(3.00));
     }
 }
