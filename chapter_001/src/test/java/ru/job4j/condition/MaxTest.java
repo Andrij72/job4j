d92cc8ff9fb64cf201e706f628ee73c83e59d918
Andrij72 <akulmmm@gmail.com>
2019-12-24 10:16:49 +0200

    Overload the max method for three numbers[#197507]

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 
 public class MaxTest {
-
     @Test
     public void whenMax4To3tThen4() {
         int result = Max.max(4, 3);
@@ -24,4 +23,34 @@ public class MaxTest {
         int result = Max.max(2, 2);
         Assert.assertThat(result, is(2));
     }
-}
+
+    @Test
+    public void whenMax3To1To2Then3() {
+        int result = Max.max(3, 1, 2);
+        Assert.assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax4To1To3To2Then4() {
+        int result = Max.max(4, 1, 2, 3);
+        Assert.assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenMax3To3To2Then3() {
+        int result = Max.max(3, 3, 2);
+        Assert.assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax1To1To3To4Then4() {
+        int result = Max.max(1, 1, 3, 4);
+        Assert.assertThat(result, is(4));
+    }
+
+        @Test
+        public void whenMax2To2To2Then2() {
+            int result = Max.max(2, 2, 2);
+            Assert.assertThat(result, is(2));
+        }
+    }
