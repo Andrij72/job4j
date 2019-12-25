0ed86c344e8ed200d89d623b8f34f640c1594f29
Andrij72 <akulmmm@gmail.com>
2019-12-25 16:13:41 +0200

    add tracker file for task [# 197522]

--- chapter_001/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_001/src/main/java/ru/job4j/tracker/Tracker.java
@@ -2,8 +2,8 @@ package ru.job4j.tracker;
 
 public class Tracker {
     public static void main(String[] args) {
-        /*Animal animal = new Animal();
-        Predator predator = new Predator();*/
-        Tiger tiger = new Tiger();
+        Animal animal = new Animal("horse");
+        Predator predator = new Predator("eagle");
+        Tiger tiger = new Tiger("Sherhan");
     }
 }
