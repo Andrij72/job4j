566bc68d88eb2bf2f6793e59ee0b89ea6f9ab3c5
Andrij72 <akulmmm@gmail.com>
2019-12-26 13:46:23 +0200

    fixOverloading constructor[#197522]

--- chapter_001/src/main/java/ru/job4j/tracker/Animal.java
+++ chapter_001/src/main/java/ru/job4j/tracker/Animal.java
@@ -1,9 +1,12 @@
 package ru.job4j.tracker;
 
 public class Animal {
-   private String name;
+    private String name;
+
+    public Animal() {
+    }
+
     public Animal(String name) {
-        super();
         this.name = name;
         System.out.println(this.getClass());
     }
--- chapter_001/src/main/java/ru/job4j/tracker/Tracker.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.tracker;
-
-public class Tracker {
-    public static void main(String[] args) {
-        Animal animal = new Animal("horse");
-        Predator predator = new Predator("eagle");
-        Tiger tiger = new Tiger("Sherhan");
-    }
-}
