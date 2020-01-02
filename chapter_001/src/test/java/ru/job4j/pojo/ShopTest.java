6a61954588054eeb9c449a4a3da5098922f88f21
Andrij72 <akulmmm@gmail.com>
2020-01-03 00:20:08 +0200

    fix2 delete models from array[#197532]

--- chapter_001/src/test/java/ru/job4j/pojo/ShopTest.java
+++ chapter_001/src/test/java/ru/job4j/pojo/ShopTest.java
@@ -10,17 +10,44 @@ import static org.junit.Assert.*;
 public class ShopTest {
 
     @Test
-    public void whenDeleteElementWithIndex0After4After2HaveBreadEggNullNullNull() {
+    public void whenDeleteElementWithIndex0HaveBreadEggBearNullNull() {
         Product[] products = new Product[5];
         products[0] = new Product("Milk", 10);
         products[1] = new Product("Bread", 4);
         products[2] = new Product("Egg", 19);
         products[3] = new Product("Bear", 30);
         System.out.println("Before :" + Arrays.toString(products));
-        Shop.delete(products, 0);
-        Shop.delete(products, 4);
+        Product[] result = Shop.delete(products, 0);
+        System.out.println("After :" + Arrays.toString(result));
+        System.out.println();
+        assertThat(Arrays.equals(result, products), is(true));
+    }
+
+    @Test
+    public void whenDeleteElementWithIndex4HaveMilkBreadEggBearNull() {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        products[3] = new Product("Bear", 30);
+        System.out.println("Before :" + Arrays.toString(products));
+        Product[] result = Shop.delete(products, 4);
+        System.out.println("After :" + Arrays.toString(result));
+        System.out.println();
+        assertThat(Arrays.equals(result, products), is(true));
+    }
+
+    @Test
+    public void whenDeleteElementWithIndex2HaveMilkBreadBearNullNull() {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        products[3] = new Product("Bear", 30);
+        System.out.println("Before :" + Arrays.toString(products));
         Product[] result = Shop.delete(products, 2);
         System.out.println("After :" + Arrays.toString(result));
+        System.out.println();
         assertThat(Arrays.equals(result, products), is(true));
     }
 }
