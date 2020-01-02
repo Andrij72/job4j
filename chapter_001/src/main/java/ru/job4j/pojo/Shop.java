7a6767301eab6a67e374f1eb6d96b020fdd5e00f
Andrij72 <akulmmm@gmail.com>
2020-01-02 21:13:47 +0200

    Removing models from the array[# 197532]

---
+++ chapter_001/src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,41 @@
+package ru.job4j.pojo;
+
+public class Shop {
+
+    public static void main(String[] args) {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        products[3] = new Product("Bear", 30);
+        System.out.println("Start array products:");
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+        System.out.println();
+        System.out.println("Finish array products after delete products[2]:");
+        Shop.delete(products, 0);
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+    }
+
+    public static Product[] delete(Product[] products, int index) {
+        products[index] = null;
+        for (int i = index; i < products.length - 1; i++) {
+            Product product = products[i];
+            products[i] = products[i + 1];
+        }
+        return products;
+    }
+}
