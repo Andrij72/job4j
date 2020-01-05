207857958f740463d66cadbd6bdc7cef3703f0e7
Andrij72 <akulmmm@gmail.com>
2020-01-05 02:07:19 +0200

    Scanner and reading from the console[#197485]

---
+++ chapter_002/src/main/java/ru/job4j/io/Matches.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Matches {
+    public static void main(String[] args) {
+        int count = 11;
+        Scanner input1 = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+        boolean run = true;
+        while (run) {
+            System.out.println("First user,  please take your matches 1...3" + "\n");
+            input1.hasNext();
+            int user1 = Integer.valueOf(input1.nextInt());
+            count -= user1;
+            System.out.println("There left  <<" + count + ">>  mathers");
+            if (count == 0) {
+                System.out.println("First user is winner");
+                run = false;
+            }
+            System.out.println("Second user,  please take your matches 1...3" + "\n");
+            input1.hasNext();
+            int user2 = Integer.valueOf(input1.nextInt());
+            count -= user2;
+            System.out.println("There left  <<" + count + ">>  mathers");
+            if (count == 0) {
+                System.out.println("Second user is winner");
+                run = false;
+            }
+        }
+    }
+}
