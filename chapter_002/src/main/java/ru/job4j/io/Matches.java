118f7d75d4bfb55ecc8cbf51edba042ebe20b0e2
Andrij72 <akulmmm@gmail.com>
2020-01-05 16:05:11 +0200

    2.1. clean code Realisation class StartUI[#197480]

--- chapter_002/src/main/java/ru/job4j/io/Matches.java
+++ chapter_002/src/main/java/ru/job4j/io/Matches.java
@@ -3,29 +3,29 @@ package ru.job4j.io;
 import java.util.Scanner;
 
 public class Matches {
+    private static int count = 11;
+    static boolean run = true;
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int count = 11;
-        Scanner input1 = new Scanner(System.in);
-        boolean run = true;
         while (run) {
-            System.out.println("First user,  please take your matches 1...3" + "\n");
-            input1.hasNext();
-            int user1 = Integer.valueOf(input1.nextInt());
-            count -= user1;
-            System.out.println("There left  <<" + count + ">>  mathers");
-            if (count == 0) {
-                System.out.println("First user is winner");
-                run = false;
-            }
-            System.out.println("Second user,  please take your matches 1...3" + "\n");
-            input1.hasNext();
-            int user2 = Integer.valueOf(input1.nextInt());
-            count -= user2;
-            System.out.println("There left  <<" + count + ">>  mathers");
-            if (count == 0) {
-                System.out.println("Second user is winner");
-                run = false;
-            }
+            int user1 = 0;
+            int user2 = 0;
+            checkWinner("user1");
+            checkWinner("user2");
         }
     }
+
+    public static void checkWinner(String user) {
+        System.out.println("Please, " + user + "take your matches 1...3" + "\n");
+        input.hasNext();
+        int value = Integer.valueOf(input.nextInt());
+        count -= value;
+        System.out.println("There left  <<" + count + ">>  mathers");
+        if (count <= 0) {
+            System.out.println(user + " is winner");
+            run = false;
+        }
+    }
+
 }
