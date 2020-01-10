91353ab1ef1563d05e1215ce683e9ce5781be4dc
Andrij72 <akulmmm@gmail.com>
2020-01-10 15:46:27 +0200

    5.1. Testing. Date preparing.[#197493]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -17,4 +17,31 @@ public class StartUITest {
          Item expected = new Item("I'm stajor as yet");
        assertThat(result.getName(), is(expected.getName()) );
     }
+
+    @Test
+    public void replaceItem() {
+       Tracker tracker = new Tracker();
+       Item item = new Item("My new work");
+       tracker.add(item);
+       String [] answers = {item.getId(), "Replaced item"};
+        Input input = new StubInput(answers);
+        StartUI.replaceItem(input, tracker);
+       String result = tracker.findAll()[0].getName();
+        assertThat(result, is(answers[1]));
+    }
+
+    @Test
+    public void deleteItem() {
+        Tracker tracker = new Tracker();
+        Item perfect = new Item("My perfect new work");
+        Item old = new Item("My old work");
+        tracker.add(old);
+        tracker.add(perfect);
+        String [] answersPft = {perfect.getId()};
+        String [] answersOld ={old.getId()};
+        Input inputOld = new StubInput(answersOld);
+        StartUI.deleteItem(inputOld, tracker);
+        String rsl = tracker.findAll()[0].getName();
+        assertThat(rsl, is(perfect.getName()));
+    }
 }
