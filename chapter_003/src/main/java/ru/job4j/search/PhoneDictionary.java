ff8d180af71c3c552d50776cdc3c556782f593b8
Andrij72 <akulmmm@gmail.com>
2020-01-28 21:48:18 +0200

    1. PhoneDictionary[#197452]

---
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,26 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     *
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public List<Person> find(String key) {
+        return persons
+                .stream()
+                .filter(n -> n.getName().contains(key) || n.getSurname().contains(key) || n.getAddress().contains(key) || n.getPhone().contains(key))
+                .collect(Collectors.toList());
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.
+                add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        phones.
+                add(new Person("Andrew", "Kulynych", "4533943", "Minsk"));
+
+        List<Person> persons = phones.find("nsk");
+        assertThat(persons.get(1).getSurname(), is("Kulynych"));
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+}
