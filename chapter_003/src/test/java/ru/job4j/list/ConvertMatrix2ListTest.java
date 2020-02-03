96dde66f0f677678c1f63416cfe37181f66aecd0
Andrij72 <akulmmm@gmail.com>
2020-02-04 00:30:09 +0200

    4. convert to ArrayList[#197454]

---
+++ chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConvertMatrix2ListTest {
+    @Test
+    public void when2on2ArrayThenList4() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4
+        );
+        List<Integer> result = list.toList(input);
+        System.out.println(Collections.singletonList(result));
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when3on3ArrayThenList9() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 0}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4, 5, 6, 7, 8, 0
+        );
+        List<Integer> result = list.toList(input);
+        System.out.println(Collections.singletonList(result));
+        assertThat(result, is(expect));
+    }
+}
