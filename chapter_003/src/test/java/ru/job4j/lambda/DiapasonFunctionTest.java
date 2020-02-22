03e4fc23526c00e972a8ea924d96d4e466f8d371
Andrij72 <akulmmm@gmail.com>
2020-02-23 01:45:22 +0200

    1. Calculate of function in range [#197324]

---
+++ chapter_003/src/test/java/ru/job4j/lambda/DiapasonFunctionTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static ru.job4j.lambda.DiapasonFunction.diapason;
+
+public class DiapasonFunctionTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        List<Double> result = diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(21D, 25D, 29D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenQuadraticFunction() {
+        List<Double> result = diapason(1, 4, x -> Math.pow(x, 2.0) + 1);
+        List<Double> expected = Arrays.asList(5D, 17D, 37D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenLogarithmicFunction() {
+        List<Double> result = diapason(1, 3, x -> Math.log(x + 3));
+        List<Double> expected = Arrays.asList(1.6094379124341003D, 1.9459101490553132D);
+        assertThat(result, is(expected));
+    }
+}
