45fd5fcd082cf773eb2ad7c4dbbd0a0ee777cf7e
Andrij72 <akulmmm@gmail.com>
2020-02-27 00:34:13 +0200

    modify Transform matrix numbers to list number [#197336]

--- chapter_003/src/main/java/ru/job4j/stream/MapTransform.java
+++ chapter_003/src/main/java/ru/job4j/stream/MapTransform.java
@@ -1,15 +1,18 @@
 package ru.job4j.stream;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class MapTransform<Students> {
 
     private static List<Student> students = Arrays.asList(new Student("Garry Ford", 30),
-                new Student("John Lee", 20),   new Student("Stiv Makkain", 20));
+                new Student("John Lee", 20),  new Student("John Lee", 20),  new Student("Stiv Makkain", 20));
 
     public static void main(String[] args) {
-        System.out.println(students.stream().distinct().collect(Collectors.toMap(student->student.getName(), student -> student)));
+        Set<String> set = new HashSet<>(students.size());
+        System.out.println(students.stream().filter(p -> set.add(p.getName())).collect(Collectors.toMap(student->student.getName(), student -> student)));
     }
 }
