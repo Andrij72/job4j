c6fe0e6dab91e323b7971183c123cc6b0acc054f
Andrij72 <akulmmm@gmail.com>
2020-02-27 22:40:36 +0200

    modify(identity fnc) Transform List в Map.[#197335]

--- chapter_003/src/main/java/ru/job4j/stream/MapTransform.java
+++ chapter_003/src/main/java/ru/job4j/stream/MapTransform.java
@@ -1,18 +1,27 @@
 package ru.job4j.stream;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class MapTransform<Students> {
 
     private static List<Student> students = Arrays.asList(new Student("Garry Ford", 30),
-                new Student("John Lee", 20),  new Student("John Lee", 20),  new Student("Stiv Makkain", 20));
+            new Student("John Lee", 20), new Student("John Lee", 20),
+            new Student("Stiv Makkain", 40));
+
+    public static Map<String, Student> ConvertListToMap(List<Student> students) {
+        return students.stream().
+                collect(Collectors.toMap(Student::getName, Function.identity(),(existing, replacement) -> existing));
+    }
+
+    public static Map<String, Student> listToMapWithDupKeyError(List<Student> students) {
+        return students.stream().collect(Collectors.toMap(Student::getName, Function.identity()));
+    }
 
     public static void main(String[] args) {
         Set<String> set = new HashSet<>(students.size());
-        System.out.println(students.stream().filter(p -> set.add(p.getName())).collect(Collectors.toMap(student->student.getName(), student -> student)));
+        System.out.println(students.stream().filter(p -> set.add(p.getName())).collect(Collectors.toMap(student -> student.getName(), student -> student)));
+
     }
 }
