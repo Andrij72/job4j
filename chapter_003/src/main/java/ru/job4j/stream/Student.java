b90ebc3594e61b016adf55face58ffac292adc15
Andrij72 <akulmmm@gmail.com>
2020-03-01 19:47:31 +0200

    0. Stream API improvements[#197338]

--- chapter_003/src/main/java/ru/job4j/stream/Student.java
+++ chapter_003/src/main/java/ru/job4j/stream/Student.java
@@ -1,6 +1,10 @@
 package ru.job4j.stream;
 
-public class Student {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Student implements Comparable<Student> {
     int score;
     String name;
 
@@ -20,5 +24,14 @@ public class Student {
     @Override
     public String toString() {
         return "{" + name + "[" + score + "]" + "}";
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return Integer.compare(this.getScore(), o.getScore());
+    }
+
+    public List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream().sorted().flatMap(Stream::ofNullable).takeWhile(st -> st.score > bound).collect(Collectors.toList());
     }
 }
