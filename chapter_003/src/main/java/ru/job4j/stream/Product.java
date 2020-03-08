03ca8385b0e349901c41f5067371fbfdb2c0b4bb
Andrij72 <akulmmm@gmail.com>
2020-03-08 23:39:00 +0200

    some testing Stream API

---
+++ chapter_003/src/main/java/ru/job4j/stream/Product.java
@@ -0,0 +1,32 @@
+package ru.job4j.stream;
+
+public class Product {
+    String name;
+    int price;
+
+    public Product(int price, String name) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + name +  price + '}';
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/stream/TestStream.java
@@ -0,0 +1,82 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class TestStream {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("a1", "b2", "c3", "a1");
+        Map list = strings.stream().collect(Collectors.groupingBy((p) -> p.substring(0, 0)));
+        list.forEach((k, v) -> System.out.println(k + " Value: " + v));
+
+
+        List<Product> productList = Arrays.asList(new Product(23, "potatoes"),
+                new Product(14, "orange"), new Product(13, "lemon"),
+                new Product(23, "bread"), new Product(13, "sugar"));
+
+        String listToString = productList.stream().map(Product::getName)
+                .collect(Collectors.joining(", ", "[", "]"));
+        System.out.println(listToString);
+
+        double averagePrice = productList.stream()
+                .collect(Collectors.averagingInt(Product::getPrice));
+        System.out.println(averagePrice);
+// intStream
+        IntStream intStream = IntStream.range(1, 3);
+        LongStream longStream = LongStream.rangeClosed(1, 3);
+
+// Person list
+        List<Person> persons =
+                Arrays.asList(
+                        new Person("Max", 18),
+                        new Person("Peter", 23),
+                        new Person("Pamela", 23),
+                        new Person("David", 12),
+                        new Person("Gorge", 12));
+        List<Person> filtered =
+                persons
+                        .stream()
+                        .filter(p -> p.name.startsWith("P"))
+                        .collect(Collectors.toList());
+
+        System.out.println(filtered);
+
+        // Peoples group by age
+        Map<Integer, List<Person>> personsByAge = persons
+                .stream()
+                .collect(Collectors.groupingBy(p -> p.age));
+
+        personsByAge.
+                forEach((age, p) -> System.out.format("age %s: %s\n", age, p));
+
+// transform flow elements to display
+        System.out.println("++++++++++++");
+        Map<Integer, String> map = persons
+                .stream()
+                .collect(Collectors.toMap(
+                        p -> p.age,
+                        p -> p.name,
+                        (name1, name2) -> "[" + name1 + ";" + name2 + "]"));
+        System.out.println(map);
+
+        //++++++++++++++
+        System.out.println("++++++++++++");
+        Collector<Person, StringJoiner, String> personNameCollector =
+                Collector.of(
+                        () -> new StringJoiner(" | "),          // supplier
+                        (j, p) -> j.add(p.name.toUpperCase()),  // accumulator
+                        (j1, j2) -> j1.merge(j2),               // combiner
+                        StringJoiner::toString);                // finisher
+
+        String names = persons
+                .stream()
+                .collect(personNameCollector);
+        System.out.println(names);
+    }
+}
